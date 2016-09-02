ece41c42be74857a5dd1b108f7a231a5ed04d295
bm993a <bm993a@users.noreply.github.com>
2016-09-02 18:40:51 -0500

    Update channels.py

--- channels.py
+++ channels.py
@@ -1,1 +1,20 @@
+#
+#
+#
+#
 
+import json
+import urllib
+
+# GET /channels/:channel	Get channel object
+# GET /channels/:channel/videos	Get channel's list of videos
+# GET /channels/:channel/follows	Get channel's list of following users
+# GET /channels/:channel/editors	Get channel's list of editors
+# 
+# API requiring OAuth are not included
+
+#
+#
+# return a dictionary of the JSON response
+def channel_info(channel):
+  pass
