362e5eeb958f95d0055d5d73186b7b23ab08bc3f
bm993a <bm993a@users.noreply.github.com>
2016-09-09 18:46:18 -0500

    Update ptwitch.py
    added subparsing

--- ptwitch.py
+++ ptwitch.py
@@ -2,14 +2,31 @@ import argparse
 import api
 
 parser = argparse.ArgumentParser(description='Twitch API via command line')
-
-
 parser.add_argument('action', type=str, help='which api request to preform')
+subparsers = parser.add_subparsers(dest='subcommand')
 
 
+# related to api.channels_follows
+p_channel_follows = subparses.add_parser('channels_follows')
+p_channel_follows = add.argument('-l', '--limit', action='store_true', type=int,
+  help='max number of results')
+p_channel_follows = add.argument('-c', '--cursor', action='store_true',
+  help='link from a previous request for pagination)
+p_channel_follows = add.argument('-d', '--direction', choices=['asc', 'desc'], action='store_true',
+  help='sorting based on creation date')
 
-parser.parse_args()
+# related to api.channels_info
+p_channel_info = subparsers.add_parser('channels_info')
+p_channel_info.add_argument('channel', type=string, help='twitch channl id')
 
-# #
-# parser.add_argument('name')
-# parser.add_argument('--channel', action='store_true')
+
+# setup is done - parse the command
+args = parser.parse_args()
+
+
+if args.subcommand == 'channel_follows':
+  print(args.subcommand.channel)
+if args.subcommand == 'channel_info':
+  print(args.subcommand.limit)
+
+
